95a0da9e7665cda9b3282b7cff1ffeee33b0a212
FreddyA12 <2balvarezfreddy@gmail.com>
2025-10-04 11:59:09 -0500

    first commit

---
+++ src/test/java/com/fram/vigilapp/VigilappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fram.vigilapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VigilappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
